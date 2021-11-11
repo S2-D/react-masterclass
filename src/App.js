258664bf0e11e2e88b4d317a2accf105ac6e4cc5
Sodam Shin <damsoshin@gmail.com>
2021-11-11 14:21:59 +0900

    as

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  /* Adapting based on props */
-  background-color: ${(props) => props.bgColor};
-  height: 100px;
-  width: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 10px;
 `;
-
-/* Extending Styles :  styled(Box) */
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Text = styled.span``;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal"></Box>
-      {/* prop으로 bgColor */}
-      <Circle bgColor="tomato"></Circle>
+      <Btn>log in</Btn>
+      {/* Button 컴포넌트의 스타일은 그대로 쓰고싶지만, 태그의 종류를 a 태그로 바꾸고 싶을땐 'as' 속성 이용*/}
+      <Btn as="a" href="/">
+        log in
+      </Btn>
     </Father>
   );
 }
