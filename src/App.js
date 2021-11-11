d63236f39649e29f33aab3556ca954a33c085ec1
Sodam Shin <damsoshin@gmail.com>
2021-11-11 16:11:51 +0900

    [styled-components 끝! ]dark mode Theme

--- src/App.js
+++ src/App.js
@@ -6,50 +6,16 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 63px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotationAnimation} 2s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* span대신 Emoji를 target하면 as="p"와 관계없이 적용 & Box > Emoji만 스타일 적용!  */
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-    &:active {
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😉</Emoji> {/* 애니메이션 적용 O */}
-      </Box>
-      <Emoji as="p">😉</Emoji> {/* 애니메이션 적용 X */}
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+// theme 내부의 property name은 동일해야함!
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* theme 변경하는 방법은 추후 강의에서! */}
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
